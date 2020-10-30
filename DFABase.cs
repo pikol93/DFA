71b805e358d8daac60d91d145b1bf2a94e6aec7f
Jan Kowalski <pikol93@wp.pl>
2020-10-30 14:27:49 +0100

    Removed unused parts

--- DFABase.cs
+++ DFABase.cs
@@ -3,23 +3,16 @@ using System.Collections.Generic;
 
 namespace Pikol93.DFA
 {
-    public abstract class DFABase<TSignal, TState> : IDisposable
+    public abstract class DFATree<TSignal, TState> : IDisposable
         where TSignal : Enum
         where TState : Enum
     {
-        private const float TIME_MIN = 0.1f;
-        private const float TIME_MAX = 0.4f;
-
-        // TODO: Remove Random dependency
-        protected static readonly Random random = new Random();
-
         protected Dictionary<TState, DFAState<TSignal, TState>> States { get; private set; }
         protected TState CurrentState { get; private set; }
 
-        private float timeLeft;
-        private bool _disposed;
-
         #region IDisposable
+
+        private bool _disposed;
 
         protected virtual void Dispose(bool disposing)
         {
@@ -56,34 +49,22 @@ namespace Pikol93.DFA
             States[CurrentState].EnterState(null);
         }
 
-        // TODO: Move this to game engine implementation
-        /// <summary> Called from game engine, passes update to state </summary>
-        /// <param name="delta">Time passed since last frame.</param>
-        public void Update(float delta)
-        {
-            timeLeft -= delta;
-            if (timeLeft < 0.0)
-            {
-                timeLeft = TIME_MIN + (TIME_MAX - TIME_MIN) * (float)random.NextDouble();
-                UpdateRare();
-            }
-
-            States[CurrentState]?.UpdateState(delta);
-        }
-
         public void InvokeSignal(TSignal signal)
         {
             DFATransition<TState> transition = States[CurrentState].GetTransition(signal);
-            ChangeState(transition.TargetState, transition.GetArguments?.Invoke());
-        }
-
-        private void UpdateRare()
-        {
-            States[CurrentState]?.UpdateRare();
+            
+            if (transition != null)
+            {
+                object[] args = transition.GetArguments?.Invoke();
+                ChangeState(transition.TargetState, args);
+            }
         }
 
         private void ChangeState(TState targetState, object[] args)
         {
+            if (!States.ContainsKey(targetState))
+                return;
+            
             States[CurrentState]?.ExitState();
             States[targetState]?.EnterState(args);
 
--- DFAState.cs
+++ DFAState.cs
@@ -3,23 +3,16 @@ using System.Collections.Generic;
 
 namespace Pikol93.DFA
 {
-    public abstract class DFAState<TSignal, TState> : IDisposable 
+    public abstract class DFAState<TSignal, TState> : IDisposable
         where TSignal : Enum
         where TState : Enum
     {
-        public Dictionary<TSignal, DFATransition<TState>> transitions;
+        private Dictionary<TSignal, DFATransition<TState>> Transitions { get; }
+            = new Dictionary<TSignal, DFATransition<TState>>();
+
+        #region IDisposable
 
         private bool _disposed;
-
-        public virtual void EnterState(object[] args) { }
-        public virtual void UpdateState(float delta) { }
-        public virtual void UpdateRare() { }
-        public virtual void ExitState() { }
-
-        internal DFATransition<TState> GetTransition(TSignal signal)
-        {
-            return transitions[signal];
-        }
 
         protected virtual void Dispose(bool disposing)
         {
@@ -27,7 +20,7 @@ namespace Pikol93.DFA
             {
                 if (disposing)
                 {
-                    transitions.Clear();
+                    Transitions.Clear();
                 }
                 _disposed = true;
             }
@@ -38,5 +31,29 @@ namespace Pikol93.DFA
             Dispose(true);
             GC.SuppressFinalize(this);
         }
+
+        #endregion
+
+        protected void AddTransitions(IEnumerable<KeyValuePair<TSignal, DFATransition<TState>>> transitions)
+        {
+            foreach (KeyValuePair<TSignal, DFATransition<TState>> transition in transitions)
+            {
+                Transitions.Add(transition.Key, transition.Value);
+            }
+        }
+
+        internal DFATransition<TState> GetTransition(TSignal signal)
+        {
+            try
+            {
+                return Transitions[signal];
+            }
+            catch (KeyNotFoundException) { /* Transition not found */ }
+
+            return null;
+        }
+
+        public virtual void EnterState(object[] args) { }
+        public virtual void ExitState() { }
     }
 }
